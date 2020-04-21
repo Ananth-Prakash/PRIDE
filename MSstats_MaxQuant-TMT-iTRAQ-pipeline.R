8d868ab43de6d8dd113cddce3ffc58f0640ecb87
Ananth-Prakash <33653685+Ananth-Prakash@users.noreply.github.com>
2020-04-21 10:56:35 +0100

    Add files via upload

--- MSstats_MaxQuant-TMT-iTRAQ-pipeline.R
+++ MSstats_MaxQuant-TMT-iTRAQ-pipeline.R
@@ -23,7 +23,7 @@ library(dplyr)
 library(reshape2)
 library(tidyr)
 
-setwd('/Users/ananth/Documents/MaxQuant_Bechmarking/Human/PXD012203_Ananth/')
+setwd('/Users/ananth/Documents/MaxQuant_Bechmarking/Human/PXD012203_Reanalysis/')
 
 
 #### 1. Read proteinGroups to get proteinID information.
@@ -37,7 +37,11 @@ evidence_file  <- read.table( "evidence.txt" , quote = "\"", header = TRUE, sep 
 #### 3. Read annotation file
 #### Note: In the user defined the annotation file the RAW file names in coloumn 'Run' should not have .raw extension!
 ####       the Channel coloumn is case sensitive, and entries must only be in the format 'channel.1', 'channel.2', 'channel.3', ......
-annot <- read.table("MSstat_annot.txt" , quote = "\"", header = TRUE, sep = "\t", stringsAsFactors = FALSE, comment.char = "#")
+annot <- read.table("MSstat_annot_factors.txt" , quote = "\"", header = TRUE, sep = "\t", stringsAsFactors = FALSE, comment.char = "#")
+
+
+##### 4. Read sample replicate information
+sample_replicates <- read.table("sample_replicates.txt" , quote = "\"", header = TRUE, sep = "\t", stringsAsFactors = FALSE, comment.char = "#")
 
 
 #####A. First we filter the proteinGroups input file before processing with MSstatsTMT
@@ -47,7 +51,24 @@ proteinGroups_inp <- proteinGroups_inp[ proteinGroups_inp[, "Potential.contamina
 proteinGroups_inp <- proteinGroups_inp[ proteinGroups_inp[, "Peptides"] > 1, ]
 
 
-#####B. Add gene name and protein name
+# # Only consider those protein groups for which at least 50% of the sample replicates have non-zero intensity values
+#foo <-  proteinGroups_inp
+for(i in 1:nrow(sample_replicates))
+{
+  sample_replicates[i,] <- gsub("^","Reporter.intensity.corrected.", sample_replicates[i,], perl=TRUE)
+  
+  subdata <- proteinGroups_inp[, c(unname(unlist(sample_replicates[i,-1])))]
+  proteinGroups_inp$replicate_nonzero_instensity_count <- apply(subdata[1:ncol(subdata)], 1, function(x) length(which(x != 0)) )
+  
+  proteinGroups_inp <- proteinGroups_inp[proteinGroups_inp$replicate_nonzero_instensity_count >= ncol(subdata)/2,]
+  
+}
+
+#####B. Replace missing values of NaN with NA
+
+
+
+#####C. Add gene name and protein name
 #### perform protein group to gene mapping
 #### this loop will take some time
 
@@ -82,7 +103,6 @@ for(i in 1:nrow(proteinGroups_inp))
     proteinGroups_inp[ i , "unique.gene.count"] <- str_count(unique(temp_symb), ";")+1
   }
 } #end of for
-#} #end of else
 
 
 #####C Protein groups that are mapped to a unique gene
@@ -91,7 +111,8 @@ proteinGroups_inp <- proteinGroups_inp[ proteinGroups_inp[, "unique.gene.count"]
 
 #### Start MSstatsTMT
 #### I . Convert input files into MSstat format and do initial filtering
-input.mq <- MaxQtoMSstatsTMTFormat(evidence_file, proteinGroups_inp, annot)
+input.mq <- MaxQtoMSstatsTMTFormat(evidence_file, proteinGroups_inp, annot,
+                                   rmPSM_withMissing_withinRun = TRUE)
 
 #### II Protein summarization.
 quant.msstats <- proteinSummarization(input.mq,
@@ -104,32 +125,89 @@ quant.msstats <- proteinSummarization(input.mq,
 #### If comparisons have to be made between certain conditions then specify
 #### Check how many conditions are there to be compared. 
 levels(quant.msstats$Condition)
-# [1] "Alzheimers"   "Non_demented"
+# [1] "EarlyOnset_Alzheimer's"  "EarlyOnset_Non_demented" "LateOnset_Alzheimer's"   "LateOnset_Non_demented"
 
 #### The numerator is set to 1 and denominator is set to -1
 #### All other places in the matrix related to other conditions are set to 0
-#### Here for example Alzheimers is set to denominator and hence -1
-comparison<-matrix(c(-1,1),nrow=1)
+#### Here for example betwenn EarlyOnset group Alzheimer's is set to denominator and hence -1
+comparison_1 <- matrix(c(-1,1,0,0),nrow=1)
+#For LateOnset group Alzheimer's is set to denominator
+comparison_2 <- matrix(c(0,0,-1,1),nrow=1)
 
 #### Set the column names. (Has to be in the same order as levels(quant.msstats$Condition) )
-colnames(comparison) <- levels(quant.msstats$Condition)
+colnames(comparison_1) <- levels(quant.msstats$Condition)
+colnames(comparison_2) <- levels(quant.msstats$Condition)
 
 #### Set the row name for the matrix
-row.names(comparison)<-"Non_demented / Alzheimers"
+row.names(comparison_1)<-"EarlyOnset_Non_demented / EarlyOnset_Alzheimer's"
+row.names(comparison_2)<-"LateOnset_Non_demented / LateOnset_Alzheimer's"
 
 
-test.comparison <- groupComparisonTMT(data = quant.msstats, contrast.matrix = comparison)
+test.comparison_1 <- groupComparisonTMT(data = quant.msstats, contrast.matrix = comparison_1)
+test.comparison_2 <- groupComparisonTMT(data = quant.msstats, contrast.matrix = comparison_2)
 
 
 # The output (test.comparison) has multiple entries of same proteins because of their association with multiple protein groups.
 # To filter out duplicate entries and to retain only one protein group, we retain that entry which has all the proteins that are also the majority proteins
 # ie., those proteins that have > 50% of the peptides belonging to that protein group.
-deduped_proteingroups <- merge(x=test.comparison, y=data.frame(proteinGroups_inp[,c('Majority.protein.IDs','ENSG','Gene.Symbol','Gene.Name')]),
+deduped_proteingroups_1 <- merge(x=test.comparison_1, y=data.frame(proteinGroups_inp[,c('Majority.protein.IDs','ENSG','Gene.Symbol','Gene.Name')]),
                                by.x=c('Protein'), by.y=c('Majority.protein.IDs'),
                                all.x=FALSE, all.y=FALSE) 
-  
-test.comparison.names.genecount.pvalue.filtered <- deduped_proteingroups[deduped_proteingroups$adj.pvalue <= 0.05,]
 
-write.table(test.comparison, "Protein_log2FoldChange_pvalue.txt", sep = "\t", row.names = FALSE, quote = FALSE )
-write.table(deduped_proteingroups, "Protein_log2FoldChange_pvalue-deduped_proteingroups.txt", sep = "\t", row.names = FALSE, quote = FALSE )
-write.table(test.comparison.names.genecount.pvalue.filtered, "Protein_log2FoldChange_pvalue-filtered.txt", sep = "\t", row.names = FALSE, quote = FALSE )
+deduped_proteingroups_2 <- merge(x=test.comparison_2, y=data.frame(proteinGroups_inp[,c('Majority.protein.IDs','ENSG','Gene.Symbol','Gene.Name')]),
+                                 by.x=c('Protein'), by.y=c('Majority.protein.IDs'),
+                                 all.x=FALSE, all.y=FALSE) 
+
+
+group1 <- deduped_proteingroups_1[,c("Protein", "ENSG", "Gene.Symbol", "Gene.Name", "log2FC", "adj.pvalue")]
+colnames(group1) <- c("Protein", "Gene ID", "Gene Name", "Gene description", 
+                      " 'EarlyOnset_Non_demented' vs 'EarlyOnset_Alzheimer's'.foldChange",
+                      " 'EarlyOnset_Non_demented' vs 'EarlyOnset_Alzheimer's'.pValue")
+
+group2 <- deduped_proteingroups_2[,c("Protein", "ENSG", "Gene.Symbol", "Gene.Name", "log2FC", "adj.pvalue")]
+colnames(group2) <- c("Protein", "Gene ID", "Gene Name", "Gene description", 
+                      " 'LateOnset_Non_demented' vs 'LateOnset_Alzheimer's'.foldChange",
+                      " 'LateOnset_Non_demented' vs 'LateOnset_Alzheimer's'.pValue")
+
+
+combined_foldchange_file <- merge(x=group1, y=group2,
+                                  by.x=c("Protein", "Gene ID", "Gene Name", "Gene description"),
+                                  by.y=c("Protein", "Gene ID", "Gene Name", "Gene description"),
+                                  all.x=FALSE, all.y=FALSE) 
+
+
+write.table(combined_foldchange_file, "PXD012203_FoldChange_groupcomparison.txt", sep = "\t", row.names = FALSE, quote = FALSE )
+
+
+
+
+
+# ####### old code
+# #### If comparisons have to be made between certain conditions then specify
+# #### Check how many conditions are there to be compared. 
+# levels(quant.msstats$Condition)
+# # [1] "Alzheimers"   "Non_demented"
+#
+# #### The numerator is set to 1 and denominator is set to -1
+# #### All other places in the matrix related to other conditions are set to 0
+# #### Here for example Alzheimers is set to denominator and hence -1
+# comparison<-matrix(c(-1,1),nrow=1)
+# 
+# #### Set the column names. (Has to be in the same order as levels(quant.msstats$Condition) )
+# colnames(comparison) <- levels(quant.msstats$Condition)
+# 
+# #### Set the row name for the matrix
+# row.names(comparison)<-"Non_demented / Alzheimers"
+# 
+# 
+# test.comparison <- groupComparisonTMT(data = quant.msstats, contrast.matrix = comparison)
+# 
+# 
+# # The output (test.comparison) has multiple entries of same proteins because of their association with multiple protein groups.
+# # To filter out duplicate entries and to retain only one protein group, we retain that entry which has all the proteins that are also the majority proteins
+# # ie., those proteins that have > 50% of the peptides belonging to that protein group.
+# deduped_proteingroups <- merge(x=test.comparison, y=data.frame(proteinGroups_inp[,c('Majority.protein.IDs','ENSG','Gene.Symbol','Gene.Name')]),
+#                                by.x=c('Protein'), by.y=c('Majority.protein.IDs'),
+#                                all.x=FALSE, all.y=FALSE) 
+# 
+# test.comparison.names.genecount.pvalue.filtered <- deduped_proteingroups[deduped_proteingroups$adj.pvalue <= 0.05,]
